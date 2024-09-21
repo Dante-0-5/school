5e1c3bc8fe5cad541724e8f542ddedfc2fb849c7
Dante-0-5 <simon.koechl@outlook.de>
2024-09-21 19:43:12 +0200

    Initial eclipse commit

--- java/HelloWorld/src/helloWorld/HelloWorld.java
+++ java/HelloWorld/src/helloWorld/HelloWorld.java
@@ -3,6 +3,7 @@ package helloWorld;
 public class HelloWorld {
 	public static void main (String [] args) {
 		System.out.println("Hello World!");
+		System.out.println("It's nice to meet you!");
 	}
 
 }

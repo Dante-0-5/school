42c6effd30e3a79d4df6b87bc5c214320285935f
Dante-0-5 <simon.koechl@outlook.de>
2024-09-21 19:46:36 +0200

    eclipse test

--- java/HelloWorld/src/helloWorld/HelloWorld.java
+++ java/HelloWorld/src/helloWorld/HelloWorld.java
@@ -3,7 +3,7 @@ package helloWorld;
 public class HelloWorld {
 	public static void main (String [] args) {
 		System.out.println("Hello World!");
-		System.out.println("It's nice to meet you!");
+		System.out.println("It's nice to meet you");
 	}
 
 }

f6d2622421497b136c2f537a8e93364fcdcd4283
Dante-0-5 <simon.koechl@outlook.de>
2024-10-07 14:25:38 +0200

    misc

---
+++ java/HelloWorld/src/helloWorld/Test.java
@@ -0,0 +1,15 @@
+package helloWorld;
+
+public class Test {
+	public static void main (String [] args) {
+		
+		int zahl1;
+		int zahl2;
+		int ergebnis;
+		
+		zahl1 = 1;
+		zahl2 = 2;
+		ergebnis = 0;
+	}
+
+}

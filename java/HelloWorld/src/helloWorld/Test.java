c68272ca2ac009882773857f8e844af15389798a
Dante-0-5 <simon.koechl@outlook.de>
2024-10-07 14:43:26 +0200

    Variablen und Datentypen

--- java/HelloWorld/src/helloWorld/Test.java
+++ java/HelloWorld/src/helloWorld/Test.java
@@ -2,14 +2,7 @@ package helloWorld;
 
 public class Test {
 	public static void main (String [] args) {
-		
-		int zahl1;
-		int zahl2;
-		int ergebnis;
-		
-		zahl1 = 1;
-		zahl2 = 2;
-		ergebnis = 0;
+	
 	}
 
 }
---
+++ java/berufsschule/src/berufsschule/VariablenUndDatentypen.java
@@ -0,0 +1,36 @@
+package berufsschule;
+
+public class VariablenUndDatentypen {
+	public static void main (String [] args) {
+		
+		//Deklaration
+		int zahl1;
+		int zahl2;
+		int ergebnis;
+		
+		//Initialisierung
+		zahl1 = 1;
+		zahl2 = 2;
+		ergebnis = 0;
+		
+		//Deklaration und Initialisierung im selben Schritt
+		int zahl3 = 0;
+		
+		//Datentypen:
+		
+		//boolean hält "true" oder "false"
+		boolean bool = true;
+		
+		//char hält einen einzelnen Character
+		char character = 'A';
+		//Einzelne Anführungszeichen sind wichtig!
+		
+		//byte hält ganze Zahlen von -128 - 127 (Inklusive 0!)
+		byte byte1 = 0;
+		
+		//short hält ganze Zahlen von -32768 - 32767
+		short short1 = 0;
+		
+		//int hält ganze Zahlen von
+	}
+}

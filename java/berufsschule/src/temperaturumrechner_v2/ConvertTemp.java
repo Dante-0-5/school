f4ac8abb9542f389d32092a24000f6865c005147
Dante-0-5 <simon.koechl@outlook.de>
2024-10-15 13:48:33 +0200

    Experimente mit custom methods

---
+++ java/berufsschule/src/temperaturumrechner_v2/ConvertTemp.java
@@ -0,0 +1,28 @@
+package temperaturumrechner_v2;
+
+public class ConvertTemp {
+	public static double celsius(double value, String unit) {
+		if (unit.equalsIgnoreCase("F")) {
+			return (value - 32) * (5/9);
+		} else if (unit.equalsIgnoreCase("K")) {
+			return value - 273.15;
+		}
+	}
+	
+	public static double fahrenheit(double value, String unit) {
+		if (unit.equalsIgnoreCase("C")) {
+			return (value * 1.8 + 32);
+		} else if (unit.equalsIgnoreCase("K")) {
+			return (1.8 * (value - 273.15) + 32);
+		}
+	}
+	
+	public static double kelvin(double value, String unit) {
+		if (unit.equalsIgnoreCase("C")) {
+			return value + 273.15;
+		} else if (unit.equalsIgnoreCase("F")) {
+			return (value - 32) * (5/9) + 273.15;
+		}
+	}
+
+}

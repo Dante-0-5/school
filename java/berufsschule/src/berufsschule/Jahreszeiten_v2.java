637d8445ca34a559ef85edfc5c2e5d9f542ca0f8
Dante-0-5 <simon.koechl@outlook.de>
2024-11-29 10:36:21 +0100

    Nachtrag Jahreszeiten v.2

--- java/berufsschule/src/berufsschule/Jahreszeiten_v2.java
+++ java/berufsschule/src/berufsschule/Jahreszeiten_v2.java
@@ -15,11 +15,27 @@ public class Jahreszeiten_v2 {
 			case "May":
 				System.out.println("It's Spring!");
 				break;
+				
 			case "June":
 			case "July":
 			case "August":
 				System.out.println("It's Summer!");
 				break;
+				
+			case "September":
+			case "October":
+			case "November":
+				System.out.println("It's Autumn!");
+				break;
+				
+			case "December":
+			case "January":
+			case "February":
+				System.out.println("It's Winter!");
+				break;
+				
+			default:
+				System.out.println("No valid month detected.");
 		}
 	}
 

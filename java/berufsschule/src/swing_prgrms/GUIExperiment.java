f9a093850119769816ab408b72c5070f676d3ee4
Dante-0-5 <simon.koechl@outlook.de>
2024-12-07 20:14:31 +0100

    Experiments with Swing

--- java/berufsschule/src/swing_prgrms/GUIExperiment.java
+++ java/berufsschule/src/swing_prgrms/GUIExperiment.java
@@ -1,4 +1,4 @@
-package berufsschule;
+package swing_prgrms;
 
 import javax.swing.*;
 
---
+++ java/berufsschule/src/swing_prgrms/NumberguesserGUI.java
@@ -0,0 +1,30 @@
+package swing_prgrms;
+
+import javax.swing.*;
+import java.io.*;
+
+public class NumberguesserGUI {
+	public static void defaultWindow() {
+		JFrame defaultWindow = new JFrame("Numberguesser");
+		String message = "";
+		JLabel messageLabel = new JLabel(message);
+		JTextField input = new JTextField();
+		JButton okButton = new JButton("OK");
+		
+		defaultWindow.setLayout(new GridLayout());
+		defaultWindow.add(messageLabel);
+		defaultWindow.add(input);
+		defaultWindow.setSize(600, 600);
+		defaultWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		defaultWindow.setVisible(true);
+	}
+	
+	
+	
+	
+	public static void main (String [] args) {
+		defaultWindow();
+	}
+
+}

6fbb0eec487b51be4b22a12b6e2b9415eac708cf
Dante-0-5 <simon.koechl@outlook.de>
2025-01-20 08:38:08 +0100

    numberguesser - complete

--- c++/Experiments/Numberguesser/main.cpp
+++ c++/Experiments/Numberguesser/main.cpp
@@ -7,6 +7,26 @@ int main()
 	srand(std::time(NULL));
 	int rand_nr = (rand() % 100) + 1;
 
-	
+	int guess = 0;
+
+	std::cout << "Choose a number between 1 and 100:\n";
+	std::cin >> guess;
+
+	while (guess != rand_nr) {
+		if (guess < rand_nr)
+		{
+			std::cout << "Too low! Try again!\n";
+			std::cin >> guess;
+		}
+		else if (guess > rand_nr)
+		{
+			std::cout << "Too high! Try again!\n";
+			std::cin >> guess;
+		}
+	}
+
+
+	std::cout << "Well done! You guessed corectly!";
+
 	return 0;
 }

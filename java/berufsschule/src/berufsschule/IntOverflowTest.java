7fa2b79c99c9c268c0117f4d961e46b68ee34aee
Dante-0-5 <simon.koechl@outlook.de>
2025-01-20 14:46:42 +0100

    Int Overflow Test

---
+++ java/berufsschule/src/berufsschule/IntOverflowTest.java
@@ -0,0 +1,11 @@
+package berufsschule;
+
+public class IntOverflowTest {
+	public static void main (String [] args) {
+		int toInfinity = 0;
+		for (int i=0; i>=0; i++) {
+			System.out.println(i);
+		}
+	}
+
+}

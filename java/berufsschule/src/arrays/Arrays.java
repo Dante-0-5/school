d773a7bc0c2f6beeff52c08d8d74af11e34925ed
Dante-0-5 <simon.koechl@outlook.de>
2025-03-17 14:36:51 +0100

    arrays initial commit

---
+++ java/berufsschule/src/arrays/Arrays.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class Arrays {
+	public static void main (String [] args) {
+		int zahl = 3;
+		
+		int[] feld = new int[7]; //generiert ein neues array mit 7 Feldern, ansprechbar mit [0] - [6]
+		feld[3] = 20; //gibt dem vierten Feld den Wert 20
+		
+		System.out.println(feld[3]); //gibt '20' aus
+		
+		System.out.println(feld[1]); //gibt '0' aus. Anders als bei anderen Variablen geben nicht
+									 //definierte Array-Felder nicht 'null' aus. Bei der Erstellung
+									 //(vgl. Zeile 7) werden alle Felder mit einem Standardwert
+									 //vorbelegt (im Falle int ist dieser Wert 0)
+	}
+
+}

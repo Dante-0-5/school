afca30c4062382627b8c2632640c34ea759c4a25
Dante-0-5 <simon.koechl@outlook.de>
2025-03-21 09:14:59 +0100

    arrays übungen

---
+++ java/berufsschule/src/arrays/AmazonLager.java
@@ -0,0 +1,55 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class AmazonLager {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String[][] storage = new String[3][4];
+		int row = 0;
+		int column = 0;
+		
+		storage[0][0] = "Laptop";
+		storage[0][1] = "Printer";
+		storage[0][2] = "Desktop PC";
+		storage[0][3] = "Tablet";
+		storage[1][0] = "MP3 player";
+		storage[1][1] = "iPhone";
+		storage[1][2] = "Samsung Smartphone";
+		storage[2][0] = "Monitor";
+		storage[2][1] = "Router";
+		
+		System.out.println("Please enter row [1 - 3] and column [1 - 4], separated by a whitespace. Enter 'p' to print the inventory.");
+		
+		try {
+			row = scan.nextInt();
+			column = scan.nextInt();
+			
+			System.out.println(storage[row-1][column-1]);
+			if (row == 1) {
+				System.out.println("Product is on sale.");
+			}
+		} catch (Exception InputMismatchException) {
+			if (scan.next().equalsIgnoreCase("p")) {
+				for (int i = 0; i < 3; i++) {
+					for (int j = 0; j < 4; j++) {
+						if (j != 3) {
+							if (storage[i][j] != null && storage[i][j].length() > 16) {
+								System.out.print(storage[i][j] + "\t");
+							} else if (storage[i][j] != null && storage[i][j].length() > 8) {
+								System.out.print(storage[i][j] + "\t\t");
+							} else {
+								System.out.print(storage[i][j] + "\t\t\t");
+							}
+						} else {
+							System.out.print(storage[i][j] + "\n");
+						}
+					}
+				}
+			}
+		}
+		
+	}
+
+}
---
+++ java/berufsschule/src/arrays/MehrdimensionaleArrays.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class MehrdimensionaleArrays {
+	public static void main(String[] args) {
+		int[] feld = new int[4]; 
+		//eindimensionales int-array
+		
+		int[][] feld2 = new int[4][3]; 
+		//zweidimensionales int-array mit 4 zeilen und 3 spalten
+		
+		int[][][] feld3 = new int[4][3][2]; 
+		//dreidimensionales int-array mit 4 zeilen, 3 spalten und tiefe 2
+	}
+
+}

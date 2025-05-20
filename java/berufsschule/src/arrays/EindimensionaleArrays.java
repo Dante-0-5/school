219b217951407af36266f59fa9e28f204195e44c
Dante-0-5 <simon.koechl@outlook.de>
2025-05-20 13:22:00 +0200

    eindimensionale arrays - kleiner kommentarzusatz

--- java/berufsschule/src/arrays/EindimensionaleArrays.java
+++ java/berufsschule/src/arrays/EindimensionaleArrays.java
@@ -4,7 +4,7 @@ public class EindimensionaleArrays {
 	public static void main (String [] args) {
 		int zahl = 3;
 		
-		int[] feld = new int[7]; //generiert ein neues array mit 7 Indizies, ansprechbar mit [0] - [6]
+		int[] feld = new int[7]; //generiert ein neues Array mit 7 Indizies, ansprechbar mit [0] - [6]
 		feld[3] = 20; //gibt dem vierten Index den Wert 20
 		
 		System.out.println(feld[3]); //gibt '20' aus
@@ -12,7 +12,9 @@ public class EindimensionaleArrays {
 		System.out.println(feld[1]); //gibt '0' aus. Anders als bei anderen Variablen geben nicht
 									 //definierte Array-Indizies nicht 'null' aus. Bei der Erstellung
 									 //(vgl. Zeile 7) werden alle Indizies mit einem Standardwert
-									 //vorbelegt (im Falle int ist dieser Wert 0)
+									 //vorbelegt (im Falle int ist dieser Wert 0). Ausnahme sind 
+									 //String-Arrays, da hier der vordefinierte Wert tatsächlich
+									 //null ist
 	}
 
 }

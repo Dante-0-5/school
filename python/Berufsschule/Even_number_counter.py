e47093019ef6c9aa81819f33a6d3d6b6b38f0b65
Dante-0-5 <simon.koechl@outlook.de>
2025-12-08 08:53:00 +0100

    even number counter added

---
+++ python/Berufsschule/Even_number_counter.py
@@ -0,0 +1,12 @@
+number = int(input("Enter a number: "))
+
+sum = 0
+amount = 0
+
+for i in range(0, number+1):
+	if i % 2 == 0:
+		sum += i
+		amount += 1
+
+print("Sum of all even numbers from 1 to your number: " + str(sum))
+print("Amount of all even numbers from 1 to your number: " + str(amount))

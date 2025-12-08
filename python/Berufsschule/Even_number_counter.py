6529c396772a1b567b33e8ca536320f58f716400
Dante-0-5 <simon.koechl@outlook.de>
2025-12-08 08:55:24 +0100

    even number counter - adjusted usage of sum to sum1

--- python/Berufsschule/Even_number_counter.py
+++ python/Berufsschule/Even_number_counter.py
@@ -1,12 +1,12 @@
 number = int(input("Enter a number: "))
 
-sum = 0
+sum1 = 0
 amount = 0
 
 for i in range(0, number+1):
 	if i % 2 == 0:
-		sum += i
+		sum1 += i
 		amount += 1
 
-print("Sum of all even numbers from 1 to your number: " + str(sum))
+print("Sum of all even numbers from 1 to your number: " + str(sum1))
 print("Amount of all even numbers from 1 to your number: " + str(amount))
